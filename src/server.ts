509387208f83f0b135c0a387dcf781c72490c292
Ronaldo Reis <ronaldocreis7@gmail.com>
2024-04-02 23:35:10 -0300

    feat: get attendee badge route

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,14 @@ import fastify from 'fastify';
 import { createEvent } from './routes/create-event';
 import { registerForEvent } from './routes/register-for-event';
 import { getEvent } from './routes/get-event';
+import { getAttendeeBadge } from './routes/get-attendee-badge';
 
 const app = fastify();
 
 app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
+app.register(getAttendeeBadge);
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
